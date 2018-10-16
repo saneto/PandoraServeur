2fe226ce6a0a4c7f57c4076a4cb427f68589cced
saneto <aymanezaircha@gmail.com>
2018-10-16 23:36:30 +0200

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @sql NVARCHAR(max)=''
-
-SELECT @sql += ' Drop table [' + TABLE_SCHEMA + '].['+ TABLE_NAME + ']'
-FROM   INFORMATION_SCHEMA.TABLES
-WHERE  TABLE_TYPE = 'BASE TABLE'
-
-Exec Sp_executesql @sql 
